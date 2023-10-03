45365ad47190f04b042b04ae668dd87128512beb
VikaPaz <vikaqse@gmail.com>
2023-10-03 09:56:13 +0300

    prints removed

--- main.py
+++ main.py
@@ -5,6 +5,3 @@ def get_degree(r: float, t: float, a: float, v: float = 0) -> float:
     s = v * t + (a * t ** 2) / 2
     alpha = s / (pi * r / 180)
     return round(alpha, 2)
-
-print(get_degree(2, 10, 1, 0))
-# (1432.8)
