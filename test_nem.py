6b35a80b698e93595f3dc82121c7cbeed783b6bb
Семен <trofimovsd2006@gmail.com>
2023-10-07 11:26:16 +0300

    test

--- test_nem.py
+++ test_nem.py
@@ -1,11 +1,26 @@
 import pytest
 from main import get_degree
+"""distance test.
 
+    Args:
+        rdu: float - radius
+        time: float - time
+        vel: float - velocity
+
+    Returns:
+        float - degree of rotation
+    """
 arr = (
-        ((2.0,10.0,1.0), 1432.39),
-        ((2,10,1), 1432.39),
+        ((2.0, 10.0, 1.0), 1432.39),
+        ((2, 10, 1), 1432.39),
 )
+
 
 @pytest.mark.parametrize('zna, otvet', arr)
 def test_main(zna, otvet):
     assert get_degree(*zna) == otvet
+
+@pytest.mark.parametrize('zna, otvet', arr)
+def test_exception(zna,otvet):
+    with pytest.raises(TypeError):
+        test_main( ((2, 10), 1432.39),)
