3b367bf0921289e1cf5ad1cb5713a6bfa7feda6f
sementrof <114408546+sementrof@users.noreply.github.com>
2023-10-07 11:09:58 +0300

    Add files via upload

--- test_nem.py
+++ test_nem.py
@@ -1,10 +1,20 @@
 import pytest
 from main import get_degree
+"""distance test.
 
+    Args:
+        rdu: float - radius
+        time: float - time
+        vel: float - velocity
+
+    Returns:
+        float - degree of rotation
+    """
 arr = (
-        ((2.0,10.0,1.0), 1432.39),
-        ((2,10,1), 1432.39),
+        ((2.0, 10.0, 1.0), 1432.39),
+        ((2, 10, 1), 1432.39),
 )
+
 
 @pytest.mark.parametrize('zna, otvet', arr)
 def test_main(zna, otvet):
